4b608a0dbff794a8a9f4ab0db18aafb96de3edb3
Marie J Park <hello@mariepublic.com>
2016-08-31 10:54:55 -0400

    Done

---
+++ Simon Says/Simon Says/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Marie Park on 8/31/16.
+//  Copyright © 2016 Marie Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
